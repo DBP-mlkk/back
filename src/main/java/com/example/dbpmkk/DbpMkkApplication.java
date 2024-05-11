2586d54d6f98f6694b256c65ee1344481eed6e40
ulsae <ulsae0912@gmail.com>
2024-05-11 11:35:44 +0900

    dbp-mkk

---
+++ src/main/java/com/example/dbpmkk/DbpMkkApplication.java
@@ -0,0 +1,13 @@
+package com.example.dbpmkk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DbpMkkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DbpMkkApplication.class, args);
+    }
+
+}
