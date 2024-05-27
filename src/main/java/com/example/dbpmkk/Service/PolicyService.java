f1fb22e46eb18e15a5c2cf299b9df38607ded094
rapidswap <curudow7988@gmail.com>
2024-05-27 23:39:58 +0900

    autowired 오류 해결. 404 에러

--- src/main/java/com/example/dbpmkk/Service/PolicyService.java
+++ src/main/java/com/example/dbpmkk/Service/PolicyService.java
@@ -3,12 +3,15 @@ package com.example.dbpmkk.Service;
 import com.example.dbpmkk.DAO.PolicyDao;
 import com.example.dbpmkk.Domain.PolicyEntity;
 import lombok.Setter;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 
 @Service @Setter
 public class PolicyService {
+    @Autowired
     private PolicyDao policyDao;
 
     public List<PolicyEntity> list() {
