2e30b2cafc35f5828e31e18aef9d9fd29c8db562
rapidswap <curudow7988@gmail.com>
2024-05-27 20:23:24 +0900

    Dao를 사용하여 출력하기 현재 500 에러 뜸 접속 /policies/list

--- src/main/java/com/example/dbpmkk/Controller/PolicyController.java
+++ src/main/java/com/example/dbpmkk/Controller/PolicyController.java
@@ -1,28 +1,20 @@
 package com.example.dbpmkk.Controller;
 
-import ch.qos.logback.core.model.Model;
-import com.example.dbpmkk.Domain.Entity.PolicyEntity;
 import com.example.dbpmkk.Service.PolicyService;
-import org.springframework.ui.Model;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
 
-
-import java.util.List;
-
+import lombok.AllArgsConstructor;
 
 @Controller
+@RequestMapping("policies")
+@AllArgsConstructor
 public class PolicyController {
-    @Autowired
     private PolicyService policyService;
 
-    @GetMapping("/supports")
-    public String getALlPolicies(Model model) {
-        List<PolicyEntity> policies = policyService.getAllPolicies();
-        model.addAttribute("policies",policies);
-        return "index";
+    @RequestMapping("list")
+    public void list(Model model){
+        model.addAttribute("list", policyService.list());
     }
-
-
 }
