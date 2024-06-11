069b77b67eb735d6af6f72ccfa23b88a0ad71d64
dlagksrb_00 <dlagksrb0430@naver.com>
2024-06-11 14:44:15 +0900

    1. html 파일 정리 2. 로그인 페이지 -> 메인 추후) 로그인 구현

--- src/main/java/com/example/dbpmkk/DbpMkkApplication.java
+++ src/main/java/com/example/dbpmkk/DbpMkkApplication.java
@@ -3,9 +3,10 @@ package com.example.dbpmkk;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 @EnableJpaRepositories(basePackages = "com.example.dbpmkk.Repository")
 @EntityScan(basePackages = "com.example.dbpmkk.Domain")
 public class DbpMkkApplication {
---
+++ src/main/java/com/example/dbpmkk/Repository/MemberRepository.java
@@ -0,0 +1,14 @@
+package com.example.dbpmkk.Repository;
+
+import com.example.dbpmkk.Domain.Member;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+@Repository
+public interface MemberRepository {
+    Member save(Member member);
+    Optional<Member> findById(Long id);
+    Optional<Member> findByName(String name);
+    List<Member> findall();
+}
