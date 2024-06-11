a91db4456a78c771e10022c44dd76d59f7b6e0a5
dlagksrb_00 <dlagksrb0430@naver.com>
2024-06-11 22:08:09 +0900

    힘드네요

---
+++ src/main/java/com/example/dbpmkk/Domain/EducateEntity.java
@@ -0,0 +1,26 @@
+package com.example.dbpmkk.Domain;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "educate", schema = "dbp_db")
+public class EducateEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "Course_Nuber")
+    private Integer CourseNuber;
+
+    @Column(name = "Coures_year")
+    private Integer CouresYear;
+
+    @Column(name = "Course_Name", length = 300)
+    private String CourseName;
+
+    @Column(name = "Course_Link", length = 1000)
+    private String CourseLink;
+
+}
