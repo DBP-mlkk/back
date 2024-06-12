f6dcfa7ed7402adfec00bdac3c1da50df6a845f9
rapidswap <73596570+rapidswap@users.noreply.github.com>
2024-06-12 18:36:48 +0900

    param!!

--- src/main/java/com/example/dbpmkk/Repository/PolicyEntityRepository.java
+++ src/main/java/com/example/dbpmkk/Repository/PolicyEntityRepository.java
@@ -1,9 +1,10 @@
 package com.example.dbpmkk.Repository;
 
 import com.example.dbpmkk.Domain.PolicyEntity;
-import org.apache.ibatis.annotations.Param;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
